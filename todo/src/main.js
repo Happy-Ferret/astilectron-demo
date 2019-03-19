b6161b7f64475d0be9d9076fa54a287daabf18f7
Happy-Ferret <MarkBauer@gmx.ch>
2019-03-19 15:46:47 +0100

    Add About View
    Basic, dependency-less routing.

--- todo/src/main.js
+++ todo/src/main.js
@@ -2,9 +2,40 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import AboutVue from './views/About.vue'
+
 Vue.config.productionTip = false
+
+const routes = {
+  '/': App,
+  '/about': AboutVue
+}
 
 new Vue({
   store,
-  render: h => h(App)
+  data: {
+    currentRoute: window.location.pathname
+  },
+  mounted() {
+    document.addEventListener('astilectron-ready', () => {
+      this.listen();
+    })
+  },
+  computed: {
+    ViewComponent() {
+      return routes[this.currentRoute] || App
+    }
+  },
+  methods: {
+    listen() {
+      astilectron.onMessage(function (message) {
+        switch (message.name) {
+          case "about":
+            window.open("/about");
+            break;
+        }
+      });
+    }
+  },
+  render(h) { return h(this.ViewComponent) }
 }).$mount('#app')
