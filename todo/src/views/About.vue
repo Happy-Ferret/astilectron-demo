a1c35aeeeea07e0313acd651e76d2145b4b24eb5
Happy-Ferret <MarkBauer@gmx.ch>
2019-03-19 15:46:47 +0100

    Add About View
    Basic, dependency-less routing.

---
+++ todo/src/views/About.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="about">
+    <img alt="Vue logo" src="https://cdn.chrisshort.net/testing-certificate-chains-in-go/GOPHER_MIC_DROP.png" width="25%">
+    <Todos/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about-page"
+};
+</script>
